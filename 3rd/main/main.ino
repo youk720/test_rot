56439b8c4f83c6a5c9929236f0b41eec742a107d
youk233_231 <tjm.yk0259@gmail.com>
2019-06-12 19:12:32 +0900

    2019/6/12_1

--- 3rd/main/main.ino
+++ 3rd/main/main.ino
@@ -7,7 +7,7 @@
 //   Good Performance: only the first pin has interrupt capability
 //   Low Performance:  neither pin has interrupt capability
 
-// エンコーダ接続ピン指定:2=緑, 3=茶, GND=黄
+// エンコーダ接続ピン指定
 Encoder left(2, 4);
 Encoder center(2, 7);
 Encoder right(2, 8);
@@ -18,10 +18,10 @@ long positionLeft  = -999;
 long positionRight  = -999;
 //新しいエンコーダ値のもとの変数定義
 long newCenter, newLeft, newRight;
-
+// ロータリーエンコーダで操作した時にLEDの状態を変えるための変数定義
 int red, green, blue;
 
-//各種ピン番号を定数に.
+//各種ピン番号を定義
 #define rot_left_led 3
 #define rot_center_led 5
 #define rot_right_led 6
@@ -34,7 +34,9 @@ int red, green, blue;
 #define center_sw A1
 #define right_sw A2
 
+// 最初にさせるだけの動作等
 void setup() {
+  // シリアル開業
   Serial.begin(9600);
 //  Serial.println("TwoKnobs Encoder Test:");
   pinMode(rot_center_led, OUTPUT);
@@ -43,6 +45,7 @@ void setup() {
   pinMode(left_sw, INPUT); //エンコーダ値リセットスイッチ
   pinMode(center_sw, INPUT);
   pinMode(right_sw, INPUT);
+  // PWM対応ピンでLED光らせているものは記入不要のため記入していない
 }
 void loop() {
   //新しいエンコーダの値を代入
@@ -52,9 +55,12 @@ void loop() {
 
   // ロータリーエンコーダが動き次第動作
   if (newCenter != positionCenter) {
+    // ロータリーエンコーダ左右の動作の処理をいれた関数起動
     center_rotation();
+    // 動作終了後に値を同一化
     positionCenter = newCenter;
   }
+  // 他のロータリーエンコーダでも上記と同様処理を実行
   if(newLeft != positionLeft){
     left_rotation();
     positionLeft = newLeft;
@@ -64,32 +70,47 @@ void loop() {
     positionRight = newRight;
   }
 
-
+// ロータリーエンコーダ値 & LED値初期化処理
   if (digitalRead(left_sw) == LOW) {
     Serial.read();
     Serial.println("Reset LEFT encoder by switch");
     // 以下でロータリーエンコーダ値を上書き(ただしエンコーダライブラリを入れていないとこの機能は使用不可)
     left.write(0);
+    red = 0;
   }
   if(digitalRead(center_sw) == LOW){
     Serial.read();
     Serial.println("Reset CENTER encoder by switch");
     center.write(0);
+    green = 0;
   }
   if(digitalRead(right_sw) == LOW){
     Serial.read();
     Serial.println("Reset RIGHT encoder by switch");
     right.write(0);
+    blue = 0;
   }
   // LED発行
   analogWrite(RedLED, red);
   analogWrite(GreenLED, green);
   analogWrite(BlueLED, blue);
-
+  
   // ロータリーLED 点滅
-  digitalWrite(rot_left_led, LOW);
-  digitalWrite(rot_center_led, LOW);
-  digitalWrite(rot_right_led, LOW);
+  if(red != 0){
+    digitalWrite(rot_left_led, LOW);
+  }else{
+    digitalWrite(rot_left_led, HIGH);
+  }
+  if(green!=0){
+    digitalWrite(rot_center_led, LOW);
+  }else{
+    digitalWrite(rot_center_led, HIGH);
+  }
+  if(blue!=0){
+    digitalWrite(rot_right_led, LOW);
+  }else{
+    digitalWrite(rot_right_led, HIGH);
+  }
 }
 
 // エンコーダ左右振り分け
